36f689793373d262236cb07db2c9f6e6663c5832
Luis Bernuy <luisbernuyfabian@gmail.com>
2024-11-20 02:34:57 -0500

    chore: improvemenst and refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { COMPONENTS } from './components';
+
+declare var $: any;
+declare var AOS: any;
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,28 @@ import { COMPONENTS } from './components';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'wedding-invitation';
+export class AppComponent implements OnInit, AfterViewInit {
+  device: string = '';
+  imageParallax: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      this.device = 'mobile';
+    } else {
+      this.device = 'desktop';
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.device == 'mobile' || $(window).width() < 768) {
+      this.imageParallax = 'img/fondo-mobile.webp';
+    } else {
+      this.imageParallax = 'img/fondo.webp';
+    }
+
+    $('.portada').parallax({ imageSrc: this.imageParallax });
+    AOS.init();
+  }
 }
